916913fe7238eb4be5b12067046148e67321aba8
CiccioFr <cicciofr.net@tiscali.it>
2023-02-14 14:33:43 +0100

    Annotation nella Classe del main()

--- src/main/java/it/cgmconsulting/mostracanina_ribatti/MostracaninaRibattiApplication.java
+++ src/main/java/it/cgmconsulting/mostracanina_ribatti/MostracaninaRibattiApplication.java
@@ -2,8 +2,9 @@ package it.cgmconsulting.mostracanina_ribatti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication @EnableAsync
 public class MostracaninaRibattiApplication {
 
 	public static void main(String[] args) {

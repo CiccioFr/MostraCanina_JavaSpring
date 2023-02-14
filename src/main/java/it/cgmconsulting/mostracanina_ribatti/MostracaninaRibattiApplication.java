f79cf3d2db5d4ea9e76666580cfded8de803fb7a
CiccioFr <cicciofr.net@tiscali.it>
2023-02-14 12:43:14 +0100

    Creazione Progetto

---
+++ src/main/java/it/cgmconsulting/mostracanina_ribatti/MostracaninaRibattiApplication.java
@@ -0,0 +1,13 @@
+package it.cgmconsulting.mostracanina_ribatti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostracaninaRibattiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostracaninaRibattiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/cgmconsulting/mostracanina_ribatti/MostracaninaRibattiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cgmconsulting.mostracanina_ribatti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostracaninaRibattiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
